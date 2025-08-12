ffa702862d365a7629710e33835b1c8d86f1d821
xhffffff <156663339+xhffffff@users.noreply.github.com>
2025-08-12 13:13:24 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# mycode
-all my code
+<img src="assets/meanflow.gif" width="2000">
+
+# MeanFlow
+
+😈 This repository offers an **unofficial PyTorch implementation** of the paper [_Mean Flows for One-step Generative Modeling_](https://arxiv.org/pdf/2505.13447), building upon [Just-a-DiT](https://github.com/ArchiMickey/Just-a-DiT) and [EzAudio](https://github.com/haidog-yaqub/EzAudio).
+
+💬 Contributions and feedback are very welcome — feel free to open an issue or pull request if you spot something or have ideas!
+
+🛠️ This codebase is kept as clean and minimal as possible for easier integration into your own projects — thus, frameworks like Wandb are intentionally excluded.
+
+## 📢 Shameless Plug (Ad Spot)
+
+🚀 Check out our recent project on stylized text-to-speech: [🧢 CapSpeech](https://wanghelin1997.github.io/CapSpeech-demo/).
+
+(Yes, this is an ad 😂)
+
+## Examples
+**MNIST** -- 10k training steps, 1-step sample result:
+
+![MNIST](assets/mnist_10k.png)
+
+**MNIST** -- 6k training steps, 1-step CFG (w=2.0) sample result:
+
+![MNIST-cfg](assets/mnist_6k_cfg2.png)
+
+**CIFAR-10** -- 200k training steps, 1-step CFG (w=2.0) sample result:
+
+![CIFAR-10-cfg](assets/cfg_200k_cfg2.png)
+
+## TODO
+- [x] Implement basic training and inference
+- [x] Enable multi-GPU training via 🤗 Accelerate
+- [x] Add support for Classifier-Free Guidance (CFG)
+- [x] Integrate latent image representation support
+- [ ] Add tricks like improved CFG mentioned in Appendix
+- [ ] Improve code clarity and structure, following 🤗 Diffusers style  
+- [ ] Extend to additional modalities (e.g., audio, speech)
+
+## Known Issues (PyTorch)
+- `jvp` is incompatible with Flash Attention and likely also with Triton, Mamba, and similar libraries.  
+- `jvp` significantly increases GPU memory usage, even when using `torch.utils.checkpoint`.
+- CFG is implemented implicitly, leading to some limitations:
+  - The CFG scale is fixed at training time and cannot be adjusted during inference.  
+  - Negative prompts are not supported, such as "noise" or "low quality" commonly used in text-to-image diffusion models.
+  
+## 🌟 Like This Project?
+If you find this repo helpful or interesting, consider dropping a ⭐ — it really helps and means a lot!
---
+++ meanflow.py
@@ -0,0 +1,213 @@
+import torch
+import torch.nn.functional as F
+from einops import rearrange
+from functools import partial
+import numpy as np
+
+
+class Normalizer:
+    # minmax for raw image, mean_std for vae latent
+    def __init__(self, mode='minmax', mean=None, std=None):
+        assert mode in ['minmax', 'mean_std'], "mode must be 'minmax' or 'mean_std'"
+        self.mode = mode
+
+        if mode == 'mean_std':
+            if mean is None or std is None:
+                raise ValueError("mean and std must be provided for 'mean_std' mode")
+            self.mean = torch.tensor(mean).view(-1, 1, 1)
+            self.std = torch.tensor(std).view(-1, 1, 1)
+
+    @classmethod
+    def from_list(cls, config):
+        """
+        config: [mode, mean, std]
+        """
+        mode, mean, std = config
+        return cls(mode, mean, std)
+
+    def norm(self, x):
+        if self.mode == 'minmax':
+            return x * 2 - 1
+        elif self.mode == 'mean_std':
+            return (x - self.mean.to(x.device)) / self.std.to(x.device)
+
+    def unnorm(self, x):
+        if self.mode == 'minmax':
+            x = x.clip(-1, 1)
+            return (x + 1) * 0.5
+        elif self.mode == 'mean_std':
+            return x * self.std.to(x.device) + self.mean.to(x.device)
+
+
+def stopgrad(x):
+    return x.detach()
+
+
+def adaptive_l2_loss(error, gamma=0.5, c=1e-3):
+    """
+    Adaptive L2 loss: sg(w) * ||Δ||_2^2, where w = 1 / (||Δ||^2 + c)^p, p = 1 - γ
+    Args:
+        error: Tensor of shape (B, C, W, H)
+        gamma: Power used in original ||Δ||^{2γ} loss
+        c: Small constant for stability
+    Returns:
+        Scalar loss
+    """
+    delta_sq = torch.mean(error ** 2, dim=(1, 2, 3), keepdim=False)
+    p = 1.0 - gamma
+    w = 1.0 / (delta_sq + c).pow(p)
+    loss = delta_sq  # ||Δ||^2
+    return (stopgrad(w) * loss).mean()
+
+
+class MeanFlow:
+    def __init__(
+        self,
+        channels=1,
+        image_size=32,
+        num_classes=10,
+        normalizer=['minmax', None, None],
+        # mean flow settings
+        flow_ratio=0.50,
+        # time distribution, mu, sigma
+        time_dist=['lognorm', -0.4, 1.0],
+        cfg_ratio=0.10,
+        # set scale as none to disable CFG distill
+        cfg_scale=2.0,
+        # experimental
+        cfg_uncond='v',
+        jvp_api='autograd',
+    ):
+        super().__init__()
+        self.channels = channels
+        self.image_size = image_size
+        self.num_classes = num_classes
+        self.use_cond = num_classes is not None
+
+        self.normer = Normalizer.from_list(normalizer)
+
+        self.flow_ratio = flow_ratio
+        self.time_dist = time_dist
+        self.cfg_ratio = cfg_ratio
+        self.w = cfg_scale
+
+        self.cfg_uncond = cfg_uncond
+        self.jvp_api = jvp_api
+
+        assert jvp_api in ['funtorch', 'autograd'], "jvp_api must be 'funtorch' or 'autograd'"
+        if jvp_api == 'funtorch':
+            self.jvp_fn = torch.func.jvp
+            self.create_graph = False
+        elif jvp_api == 'autograd':
+            self.jvp_fn = torch.autograd.functional.jvp
+            self.create_graph = True
+
+    # fix: r should be always not larger than t
+    def sample_t_r(self, batch_size, device):
+        if self.time_dist[0] == 'uniform':
+            samples = np.random.rand(batch_size, 2).astype(np.float32)
+
+        elif self.time_dist[0] == 'lognorm':
+            mu, sigma = self.time_dist[-2], self.time_dist[-1]
+            normal_samples = np.random.randn(batch_size, 2).astype(np.float32) * sigma + mu
+            samples = 1 / (1 + np.exp(-normal_samples))  # Apply sigmoid
+
+        # Assign t = max, r = min, for each pair
+        t_np = np.maximum(samples[:, 0], samples[:, 1])
+        r_np = np.minimum(samples[:, 0], samples[:, 1])
+
+        num_selected = int(self.flow_ratio * batch_size)
+        indices = np.random.permutation(batch_size)[:num_selected]
+        r_np[indices] = t_np[indices]
+
+        t = torch.tensor(t_np, device=device)
+        r = torch.tensor(r_np, device=device)
+        return t, r
+
+    def loss(self, model, x, c=None):
+        batch_size = x.shape[0]
+        device = x.device
+
+        t, r = self.sample_t_r(batch_size, device)
+
+        t_ = rearrange(t, "b -> b 1 1 1").detach().clone()
+        r_ = rearrange(r, "b -> b 1 1 1").detach().clone()
+
+        e = torch.randn_like(x)
+        x = self.normer.norm(x)
+
+        z = (1 - t_) * x + t_ * e
+        v = e - x
+
+        if c is not None:
+            assert self.cfg_ratio is not None
+            uncond = torch.ones_like(c) * self.num_classes
+            cfg_mask = torch.rand_like(c.float()) < self.cfg_ratio
+            c = torch.where(cfg_mask, uncond, c)
+            if self.w is not None:
+                with torch.no_grad():
+                    u_t = model(z, t, t, uncond)
+                v_hat = self.w * v + (1 - self.w) * u_t
+                if self.cfg_uncond == 'v':
+                    # offical JAX repo uses original v for unconditional items
+                    cfg_mask = rearrange(cfg_mask, "b -> b 1 1 1").bool()
+                    v_hat = torch.where(cfg_mask, v, v_hat)
+            else:
+                v_hat = None
+                print('error! self.w can not be None')
+
+        # forward pass
+        # u = model(z, t, r, y=c)
+        model_partial = partial(model, y=c)
+        jvp_args = (
+            lambda z, t, r: model_partial(z, t, r),
+            (z, t, r),
+            (v_hat, torch.ones_like(t), torch.zeros_like(r)),
+        )
+
+        if self.create_graph:
+            u, dudt = self.jvp_fn(*jvp_args, create_graph=True)
+        else:
+            u, dudt = self.jvp_fn(*jvp_args)
+
+        u_tgt = v_hat - (t_ - r_) * dudt
+
+        error = u - stopgrad(u_tgt)
+        loss = adaptive_l2_loss(error)
+        # loss = F.mse_loss(u, stopgrad(u_tgt))
+
+        mse_val = (stopgrad(error) ** 2).mean()
+        return loss, mse_val
+
+    @torch.no_grad()
+    def sample_each_class(self, model, n_per_class, classes=None,
+                          sample_steps=5, device='cuda'):
+        model.eval()
+
+        if classes is None:
+            c = torch.arange(self.num_classes, device=device).repeat(n_per_class)
+        else:
+            c = torch.tensor(classes, device=device).repeat(n_per_class)
+
+        z = torch.randn(c.shape[0], self.channels,
+                        self.image_size, self.image_size,
+                        device=device)
+
+        t_vals = torch.linspace(1.0, 0.0, sample_steps + 1, device=device)
+
+        # print(t_vals)
+
+        for i in range(sample_steps):
+            t = torch.full((z.size(0),), t_vals[i], device=device)
+            r = torch.full((z.size(0),), t_vals[i + 1], device=device)
+
+            # print(f"t: {t[0].item():.4f};  r: {r[0].item():.4f}")
+
+            t_ = rearrange(t, "b -> b 1 1 1").detach().clone()
+            r_ = rearrange(r, "b -> b 1 1 1").detach().clone()
+
+            v = model(z, t, r, c)
+            z = z - (t_-r_) * v
+
+        z = self.normer.unnorm(z)
+        return z
